e31d5f00875abbe944010fa1e5de8e792752907e
AISWARYA RETHITTA I <aiswaryarethitta@gmail.com>
2026-08-23 23:57:02 +0530

    Time: 6 ms (8.24%), Space: 263.3 MB (12.44%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,6 +6,6 @@ public:
         
         std::reverse(nums.begin(), nums.end());         
         std::reverse(nums.begin(), nums.begin() + k);    
-        std::reverse(nums.begin() + k, nums.end());    
+        std::reverse(nums.begin() + k, nums.end());
     }
 };
