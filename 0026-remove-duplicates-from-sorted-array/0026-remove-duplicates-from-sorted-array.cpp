ffdb137b51b94db67444c2a3260a55041f5d7c44
AISWARYA RETHITTA I <aiswaryarethitta@gmail.com>
2026-08-25 00:03:54 +0530

    Time: 0 ms (100%), Space: 22.7 MB (18.59%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,8 +3,8 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int i = 0;
 
-        for (int j = 1; j < nums.size(); j++) {
-            if (nums[i] != nums[j]) {
+        for (int j = 0; j < nums.size(); j++){
+            if (nums[i] != nums[j]){
                 i++;
                 nums[i] = nums[j];
             }
@@ -12,5 +12,6 @@ public:
         return i+1;
     }
 };
+
 
 
