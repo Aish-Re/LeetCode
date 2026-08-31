16637363a83705b07daf6664cfe32ccc7b69f0fe
AISWARYA RETHITTA I <aiswaryarethitta@gmail.com>
2026-09-01 01:13:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.37%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2)
+            return x;
+
+        int l = 1;
+        int r = x;
+        int ans;
+
+        while (l <= r){
+            long long mid = l + (r - l) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
