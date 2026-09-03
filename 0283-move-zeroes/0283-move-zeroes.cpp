13b3e5f366bac8f9540f66c07be3923e0c3d98d7
AISWARYA RETHITTA I <aiswaryarethitta@gmail.com>
2026-09-03 10:43:09 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n =  nums.size();
+        int i = -1;
 
-        vector<int> temp;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                temp.push_back(nums[i]);
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                i++;
+                nums[i] = nums[j];
             }
         }
 
-        for (int i = 0; i < temp.size(); i++) {
-            nums[i] = temp[i];
-        }
-
-        for (int i = temp.size(); i < n; i++) {
-            nums[i] = 0;
+        for (int k = i+1; k < nums.size(); k++) {
+            nums[k] = 0;
         }
     }
 };
